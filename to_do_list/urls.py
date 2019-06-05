7ecb154f62fed2204663c7aea61f6f5e11c23893
tiedaner <tiedaner666@gmail.com>
2019-06-05 16:17:48 +0800

    模板完成

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todolist.urls'))
+    path('todo/', include('todolist.urls'))
 ]
